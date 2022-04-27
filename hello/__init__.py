058ee9e5a70eed09ffc25a91e8fbd2738346984d
rahqis <rahqi@ad.unc.edu>
2022-04-27 19:06:05 -0400

    new functional tests

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,3 +4,19 @@
 def hello(who):
     """function that greats"""
     return "hello " + who
+
+
+def addx(x, y):
+    return x + y
+
+
+def subx(x, y):
+    return x-y
+
+
+def multx(x, y):
+    return x*y
+
+
+def divx(x, y):
+    return x/y
--- hello/test_hello.py
+++ hello/test_hello.py
@@ -1,6 +1,6 @@
 """sample test"""
 import unittest
-
+from __init__ import addx, subx, multx, divx
 from hello import hello
 
 
@@ -14,3 +14,18 @@ class TestHello(unittest.TestCase):
     def test_world_unicode(self):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
+
+    def test_addx(self):
+        self.assertEqual(addx(1, 1), 2)
+
+    def test_addx_neg(self):
+        self.assertEqual(addx(1, -2), -1)
+
+    def test_subx(self):
+        self.assertEqual(subx(1, 1), 0)
+
+    def test_multx(self):
+        self.assertEqual(multx(2, 2), 4)
+
+    def test_divx(self):
+        self.assertEqual(divx(6, 2), 3)
